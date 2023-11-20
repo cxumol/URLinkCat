31b23d6f10aa2d319af6d0b2ebe4710731516dfc
cxumol <cxumol@users.noreply.github.com>
2023-11-20 01:22:02 +0000

    UI: adaptive to fit many widths of devices

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,4 +11,32 @@
 	/>
 </svelte:head>
 
+<main class="app">
 <App {data} />
+</main>
+
+<style>
+/* Medium devices (tablets, 768px and up) */
+@media (min-width: 768px) {
+    main.app {
+        padding: 0 16px;
+    }
+}
+
+/* Large devices (desktops, 992px and up) */
+@media (min-width: 992px) {
+    main.app {
+        max-width: 960px; /* Adjust this value based on your design preference */
+        margin: auto; /* This centers the content */
+        /* padding: 24px; */
+    }
+}
+
+/* Extra large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) {
+    main.app {
+        max-width: 1140px; /* Adjust this value based on your design preference */
+        /* padding: 32px; */
+    }
+}
+</style>
